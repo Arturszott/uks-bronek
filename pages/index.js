13c086ea20e89ef06ed2c9b183ee1ca7fad481a2
Artur Szott <artur.szott@holidaycheck.com>
2016-08-02 18:29:23 +0200

    Dont use css urls

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import { prefixLink } from 'gatsby-helpers'
 import DocumentTitle from 'react-document-title'
 import { config } from 'config'
 
+import backgroundUrl from '../assets/bg1.png';
+
 import './home.scss'
 
 export default class Index extends React.Component {
@@ -14,6 +16,7 @@ export default class Index extends React.Component {
           <h1>
             Hi people
           </h1>
+          <img src={backgroundUrl} alt=""/>
           <p>Welcome to your new Gatsby site</p>
           <h2>Below are some pages showing different capabilities built-in to Gatsby</h2>
           <h3>Supported file types</h3>
